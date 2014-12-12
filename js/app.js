8bc6a39dac9d945ae07124a80ef8dadd89781ad4
YiyiZhang <yiyiwzhang@gmail.com>
2014-12-11 19:49:23 -0500

    made working animated panels

--- js/app.js
+++ js/app.js
@@ -1,13 +1,33 @@
 $(function(){
 
-//ANIMATED BOXES
-	$(".animatedcontainer").hover(
+//ANIMATED BOXES 
+	$(".animatedcontainer1").hover(
 		function(){//mousein
-			$(".animatedpanel").animate(
+			$(".animatedpanel1").animate(
 				{height: "30%"},600)
 			},
 		function(){//mouseout
-			$(".animatedpanel").animate(
+			$(".animatedpanel1").animate(
+				{height: "5%"},600)
+		});
+
+	$(".animatedcontainer2").hover(
+		function(){//mousein
+			$(".animatedpanel2").animate(
+				{height: "30%"},600)
+			},
+		function(){//mouseout
+			$(".animatedpanel2").animate(
+				{height: "5%"},600)
+		});
+
+	$(".animatedcontainer3").hover(
+		function(){//mousein
+			$(".animatedpanel3").animate(
+				{height: "30%"},600)
+			},
+		function(){//mouseout
+			$(".animatedpanel3").animate(
 				{height: "5%"},600)
 		});
 
